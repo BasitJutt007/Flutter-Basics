1c4e3392475dd7a1a5da78a649fc1cfd07290af3
BasitJutt007 <fayyaz855@gmail.com>
2023-03-25 01:03:12 +0500

    day4 completed

--- lib/login_page.dart
+++ lib/login_page.dart
@@ -6,8 +6,41 @@ class LoginPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Material(
-      child: Center(
-        child: Text("LoginPage"),
+      color: Colors.white,
+      child: Column(
+        children: [
+          Image.asset(
+            "assets/images/undraw_Login_re_4vu2.png",
+            fit: BoxFit.cover,
+          ),
+          SizedBox(
+            height: 20,
+          ),
+          Text(
+            "Welcome",
+            style: TextStyle(fontSize: 25, fontWeight: FontWeight.bold),
+          ),
+          Padding(
+            padding: const EdgeInsets.symmetric(vertical: 16, horizontal: 32),
+            child: Column(
+              children: [
+                TextFormField(
+                  decoration: InputDecoration(
+                      hintText: "Enter Username", labelText: "Username"),
+                ),
+                TextFormField(
+                  obscureText: true,
+                  decoration: InputDecoration(
+                      hintText: "Enter Password", labelText: "Password"),
+                ),
+                SizedBox(
+                  height: 20,
+                ),
+                ElevatedButton(onPressed: () {}, child: Text("Login"))
+              ],
+            ),
+          )
+        ],
       ),
     );
   }
--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_application_1/home_page.dart';
 import 'package:flutter_application_1/login_page.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 void main() {
   runApp(myApp());
@@ -12,9 +13,10 @@ class myApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      theme: ThemeData(fontFamily: GoogleFonts.lato().fontFamily),
       //home: HomePage(),
       routes: {
-        "/": (context) => HomePage(),
+        "/": (context) => LoginPage(),
         "/login": (context) => LoginPage()
       },
     );
